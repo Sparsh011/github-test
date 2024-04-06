db4725155d0602bb17b6021ecfc0e3ff25af8062
Sparsh Chadha <93155464+Sparsh011@users.noreply.github.com>
2024-04-06 15:08:13 +0530

    changes of A pushed

--- app/src/main/java/com/sparshchadha/githubtest/MainActivity.kt
+++ app/src/main/java/com/sparshchadha/githubtest/MainActivity.kt
@@ -35,7 +35,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "This change is made from branch A",
         modifier = modifier
     )
 }
